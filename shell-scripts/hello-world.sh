847d7fc3e4e53ab17048edd395b1f672cf62cb1f
nickassembly <nickguerra@gmail.com>
2022-12-26 07:34:58 -0600

    updated to add to file during execution

--- shell-scripts/hello-world.sh
+++ shell-scripts/hello-world.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Hello World"
+
+echo "My favorite movie is \"Limitless"\" > ./file.txt
+
+#this is a comment
+
+echo "This is a new line appended" >> ./file.txt+
+
+echo "My favorite car is Keller" >> ./file.txt
